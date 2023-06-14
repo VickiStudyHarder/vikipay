4d80ae66150a7cebdfbffea2ed87416d72ac9944
VickiStudyHarder <vickichen98@gmail.com>
2023-06-14 21:16:08 +1000

    Build collapsible component and use js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// find all element that have the collapsible class
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
